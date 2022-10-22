934ef61938a52b29c4c9d6edf0921be49fd27ffd
Ahmad Arif <arifmassih6@gmail.com>
2022-10-22 15:00:47 -0400

    some basic configuration

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HomeScreen.css';
+import Nav from './Nav';
+function HomeScreen() {
+  return (
+    <div className='home-screen'>
+        {/**Nav */}
+        <Nav />
+        {/**Banner */}
+        {/**Row */}
+        </div>
+    
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ46WkbE5HdZmQ-R34PpYNGME0dNkFmnU",
+    authDomain: "netflix-clone-45ea5.firebaseapp.com",
+    projectId: "netflix-clone-45ea5",
+    storageBucket: "netflix-clone-45ea5.appspot.com",
+    messagingSenderId: "841504893471",
+    appId: "1:841504893471:web:cffab7d63f1b966a76832a",
+    measurementId: "G-FZ44SYQQJL"
+  };
