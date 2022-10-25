e9567d431602175a223823f85ba86fda4d7af697
Ahmad Arif <arifmassih6@gmail.com>
2022-10-24 21:07:02 -0400

    finished rough version of build. Fine tuning still required

--- src/screens/PlansScreen.jsx
+++ src/screens/PlansScreen.jsx
@@ -19,6 +19,22 @@ function PlansScreen() {
 
   const user = useSelector(selectUser);
 
+  const [subscription, setSubscription] = useState(null);
+
+  useEffect(() => {
+    getDocs(collection(db, "customers", user.uid, "subscriptions")).then(
+      (querySnapshot) => {
+        querySnapshot.forEach(async (subscription) => {
+          setSubscription({
+            role: subscription.data().role,
+            current_period_start:
+              subscription.data().current_period_start.seconds,
+            current_period_end: subscription.data().current_period_end.seconds,
+          });
+        });
+      }
+    );
+  }, [user.uid]);
   useEffect(() => {
     const productsRef = query(
       collection(db, "products"),
@@ -42,17 +58,13 @@ function PlansScreen() {
 
   const loadCheckout = async (priceId) => {
     const docRef = await addDoc(
-      collection(
-        doc(collection(db, "customers"), user.uid),
-        "checkout_sessions"
-      ),
+      collection(db, "customers", user.uid, "checkout_sessions"),
       {
-        prices: priceId,
+        price: priceId,
         success_url: window.location.origin,
         cancel_url: window.location.origin,
       }
     );
-    console.log(docRef);
     onSnapshot(docRef, async (snap) => {
       const { error, sessionId } = snap.data();
 
@@ -71,17 +83,37 @@ function PlansScreen() {
   };
   return (
     <div className="plansScreen">
+      {subscription && (
+        <p>
+          Renewal Date:{" "}
+          {new Date(
+            subscription?.current_period_end * 1000
+          ).toLocaleDateString()}
+        </p>
+      )}
+
       {Object.entries(products).map(([productId, productData]) => {
         //check if users subscription is active
+        const isCurrentPackage = productData.name
+          ?.toLowerCase()
+          .includes(subscription?.role);
         return (
-          <div className="plansScreen__plan">
+          <div
+            className={`plansScreen__plan ${
+              isCurrentPackage && "plansScreen__plan--disabled"
+            }`}
+            key={productId}
+          >
             <div className="plansScreen__info">
               <h5>{productData.name}</h5>
               <h6>{productData.description}</h6>
-              <h3>{"1"}</h3>
             </div>
-            <button onClick={() => loadCheckout(productData.prices.priceId)}>
-              Subscribe
+            <button
+              onClick={() =>
+                !isCurrentPackage && loadCheckout(productData.prices.priceId)
+              }
+            >
+              {isCurrentPackage ? "Current Package" : "Subscribe"}
             </button>
           </div>
         );
